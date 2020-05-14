ed01d42c0132742d2e193eb5405e87457b2e3e49
senstivz <65320673+senstivz@users.noreply.github.com>
2020-05-13 23:58:45 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
 import telegram
-# importe suas chaves de acesso do arquivo credentials.py
-from credentials import *
+# credentials.py.sample *
 import logging
 # para logar erros
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
