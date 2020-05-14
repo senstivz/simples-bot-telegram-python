74731589c915b83a578a60a8c8c2f2c7ea76cc98
senstivz <65320673+senstivz@users.noreply.github.com>
2020-05-14 14:12:36 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,4 +8,9 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 # crie o bot
 bot = telegram.Bot(token=telegram_token)
 # mande uma mensagem
-bot.sendMessage(chat_id=chat_id, text='O bot diz oi!')
+bot.sendMessage(chat_id=chat_id, text='cantadas')
+você come rato? porque você é um gato
+você não é ladrão, mas roubou meu coração
+sua mãe é uma ostra? porque você é uma pérola
+seu pai é padeiro? porque você é um pão
+não sou manu gavassi, mas eu te quero
